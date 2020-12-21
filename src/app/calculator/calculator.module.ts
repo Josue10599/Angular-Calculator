a7bbb7d0bff1b37db42db10549f5e4b093211c59
Josue Lopes <josue10599@gmail.com>
2020-12-21 20:47:28 -0300

    Create Component

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CalculatorComponent } from './components';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  declarations: [
+    CalculatorComponent
+  ],
+  exports: [
+    CalculatorComponent
+  ],
 })
 export class CalculatorModule { }
